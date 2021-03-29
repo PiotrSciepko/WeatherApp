bc533b81f87eff770d40adbf7596ddc9b619b6f4
PiotrSciepko <piotrek.priv@gmail.com>
2021-03-29 18:39:30 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,59 @@
+const showWeatherBox = (box, location = "Warszawa") => {
+    document.querySelector("body").classList.add("loading");
+
+    const city = box.querySelector(".city__name");
+    const temp = box.querySelector(".temperature__value");
+    const pressure = box.querySelector(".pressure__value");
+    const humidity = box.querySelector(".humidity__value");
+    const wind = box.querySelector(".wind-speed__value");
+    const weatherIcon = box.querySelector(".weather__icon img");
+    const ulWeatherForecast = box.querySelector(".weather__forecast");
+    const weekDay = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
+
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=f8e7ba3672fd4af1b7d125524212803&q=${location}&days=3`)
+        .then(el => el.json())
+        .then(data => {
+            city.innerHTML = data.location.name;
+            temp.innerHTML = data.current.temp_c;
+            pressure.innerHTML = data.current.pressure_mb + " hPa";
+            humidity.innerHTML = data.current.humidity + "%";
+            wind.innerHTML = data.current.wind_kph + " km/h";
+            weatherIcon.src = data.current.condition.icon;
+            ulWeatherForecast.innerHTML = data.forecast.forecastday.map(e =>
+                `<li><span class="day">${weekDay[new Date(e.date).getDay()]}</span><img src="${e.day.condition.icon}"/>`
+                + `<span class="temperature"><span class="temperature__value">${e.day.avgtemp_c}</span>&deg;C</span></li>`)
+                .join("");
+        })
+        .then(() => document.querySelector("body").classList.remove("loading"))
+}
+
+const findCity = document.querySelector(".find-city");
+findCity.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const box = document.querySelector(".module__weather");
+    const newBox = box.cloneNode(true);
+    const city = findCity.search.value;
+    console.log(city);
+    if (city !== undefined) {
+        showWeatherBox(newBox, city);
+        box.before(newBox);
+        newBox.querySelector(".btn--close").style.display = "inherit";
+        newBox.querySelector(".btn--close").addEventListener("click", () => newBox.remove())
+        document.querySelector(".module__form").hidden = true;
+        findCity.search.value = "";
+        findCity.search.placeholder = "np. Krasnystaw";
+    }
+})
+
+document.querySelector("#add-city")
+    .addEventListener("click", el => document.querySelector(".module__form").hidden = false);
+
+document.querySelector(".module__form .btn--close")
+    .addEventListener("click", el => document.querySelector(".module__form").hidden = true);
+
+const initialBox = document.querySelector(".module__weather");
+initialBox.querySelector(".btn--close").style.display = "none";
+
+showWeatherBox(initialBox);
+
+
